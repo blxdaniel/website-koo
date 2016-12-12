36ee40b0126cb14c5155cc96708a9a7961178190
Daniel Koo <blxdaniel@gmail.com>
2016-12-11 16:03:02 -0800

    Updated website to fit the style I want.

--- server.js
+++ server.js
@@ -8,12 +8,25 @@ app.use(express.static(__dirname + '/static'))
 
 app.get('/', function (req, res, next) {
   try {
-    var html = template({ title: 'Home' })
+    template = require('jade').compileFile(__dirname + '/source/templates/homepage.jade')
+    var html = template({ title: 'Daniel Koo' })
     res.send(html)
   } catch (e) {
     next(e)
   }
 })
+
+app.get('/contact', function (req, res, next) {
+  try {
+    template = require('jade').compileFile(__dirname + '/source/templates/contactinfo.jade')
+    var html = template({ title: 'Conatct Me' })
+    res.send(html)
+  } catch (e) {
+    next(e)
+  }
+})
+
+
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
