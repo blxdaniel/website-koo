03e4069b1c83482ca025fed15d45ff645b85a297
Daniel Koo <blxdaniel@gmail.com>
2016-12-12 00:45:38 -0800

    Added jQuery to the website

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express')
   , logger = require('morgan')
   , app = express()
   , template = require('jade').compileFile(__dirname + '/source/templates/homepage.jade')
+  , $ = require('jQuery')
 
 app.use(logger('dev'))
 app.use(express.static(__dirname + '/static'))
@@ -9,6 +10,17 @@ app.use('/source/img', express.static(__dirname + "/source/img"));
 
 app.get('/', function (req, res, next) {
   try {
+    $(function(){
+      $("#command").typed({
+        strings: ["cat lab_readme.txt"],
+        typeSpeed: 10,
+        callback: function(){ foo(); }
+      });
+
+      function foo(){ console.log("Callback");
+      $(".readme").delay(300).show(0); }
+
+    });
     template = require('jade').compileFile(__dirname + '/source/templates/homepage.jade')
     var html = template({ title: 'Daniel Koo' })
     res.send(html)
@@ -30,3 +42,6 @@ app.get('/project', function (req, res, next) {
 app.listen(process.env.PORT || 3000, function () {
   console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
 })
+
+//Using this for a terminal animation
+
